5813e8158ef61428642720cee8a75cbbfd2aad52
projectEscape <escaperealityproject@gmail.com>
2019-11-02 23:37:07 +0530

    Initialized client with proxy settings for communicating to server api

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  render() {
+    return <a href="/auth/google">Login With Google</a>;
+  }
+}
+
+export default App;
